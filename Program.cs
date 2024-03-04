ba3021a101ca649326e54a02f4af3974738b3ed0
egiK1337 <117719087+egiK1337@users.noreply.github.com>
2024-03-04 14:44:10 +0300

    delete comments

--- Program.cs
+++ Program.cs
@@ -11,10 +11,6 @@
             var dirForFile1 = @"C:\Otus\TestDir1";
             var dirForFile2 = @"C:\Otus\TestDir2";
 
-            var fileName1 = "File1.txt";
-            var fileName5 = "File5.txt";
-            var fileName10 = "File10.txt";
-
             var fileSystemManager = new FileSystemManager();
 
             fileSystemManager.Create(path, TestDir1);
@@ -25,20 +21,6 @@
                 fileSystemManager.Read(fileSystemManager.Add(dirForFile1, $"File{i}.txt"));
                 fileSystemManager.Read(fileSystemManager.Add(dirForFile2, $"File{i}.txt"));
             }
-
-            //var filePath1 = fileSystemManager.Add(dirForFile1, fileName1);
-            //var filePath2 = fileSystemManager.Add(dirForFile1, fileName5);
-            //var filePath3 = fileSystemManager.Add(dirForFile1, fileName10);
-            //var filePath4 = fileSystemManager.Add(dirForFile2, fileName1);
-            //var filePath5 = fileSystemManager.Add(dirForFile2, fileName5);
-            //var filePath6 = fileSystemManager.Add(dirForFile2, fileName10);
-
-            //fileSystemManager.Read(filePath1);
-            //fileSystemManager.Read(filePath2);
-            //fileSystemManager.Read(filePath3);
-            //fileSystemManager.Read(filePath4);
-            //fileSystemManager.Read(filePath5);
-            //fileSystemManager.Read(filePath6);
         }
     }
 }
